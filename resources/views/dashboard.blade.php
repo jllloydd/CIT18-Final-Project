0e7419e63e4edceb5693b8b36994f11179d70a0f
jllloydd <wayfivet@gmail.com>
2024-04-16 20:15:25 +0800

    Add: if statement to check if tasks table is not empty within the view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -112,7 +112,7 @@
       </div>
   </div> 
 
-
+  @if($tasks->isNotEmpty())
 <!-- Edit Modal -->
   <div id="edit-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
@@ -132,7 +132,8 @@
             </div>
             <!-- Modal body -->
             <meta name="csrf-token" content="{{ csrf_token() }}">
-            <form class="p-4 md:p-5 form" action="{{ route('task.update', $task->id) }}" method="post">                @csrf
+            <form class="p-4 md:p-5 form" action="{{route('task.update', $task->id)}}" method="post">                
+                @csrf
                 @method("POST")
                 <div class="grid gap-4 mb-4 grid-cols-2">
 
@@ -204,5 +205,6 @@
         </div>
     </div>
 </div> 
+@endif
 </main>
 </x-app-layout>
